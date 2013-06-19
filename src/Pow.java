04050d7c503f9ef74e647c669738fe2261c5d8f2
goge1021 <goge1021@gmail.com>
2013-06-19 12:25:31 +0900

    Create Pow.java

---
+++ src/Pow.java
@@ -0,0 +1,23 @@
+public class Pow {
+    public double pow(double x, int n){
+    	int binaryLen = Integer.toString(n, 2).length();
+		double[] temp = new double[binaryLen];
+		temp[0] = x;
+		for(int i=1; i<binaryLen; i++){
+			temp[i]= temp[i-1] * temp[i-1];
+		}
+		double ret = 1;
+		for(int i=0; i<binaryLen; i++){
+			if((n&1) == 1){
+				ret *= temp[i];
+			}
+			n = n >> 1;
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args){
+		Pow p = new Pow();
+		System.out.print(p.pow(2,12));
+	}
+}
